383559238b483b109b9661966bf70abab62290f1
Anastasia Pankina <anastaciap@ymail.com>
2019-08-10 19:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ console.log(name)
 
 console.log("Height:")
 // console.log(height  -4) //when it is - it always tries to convert string to a number 
-console.log(parseInt(height)+2)
+console.log(parseInt(height)+2) //when it is plus it will just add number to an existing string , so you need to convert by parseInt. 
+
