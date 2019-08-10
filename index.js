7f15fa5fcce9d52b95d02c728c56e0a5645ccc7b
Anastasia Pankina <anastaciap@ymail.com>
2019-08-10 19:05:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height  -4) //when it is - it always tries to convert string to a number 
+// console.log(height  -4) //when it is - it always tries to convert string to a number 
+console.log(height+2)
