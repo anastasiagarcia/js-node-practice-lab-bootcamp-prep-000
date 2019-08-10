707385bf475b9451ed83080c0eb560035df79b3c
Anastasia Pankina <anastaciap@ymail.com>
2019-08-10 19:10:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // write your code below!
 
 var name = "Anastasia"
-// var height = "68"
-var height= 68;
+// var height = "68" //string
+var height= 68;//it is a number now 
 
 console.log("Name:")
 console.log(name)
@@ -10,5 +10,5 @@ console.log(name)
 console.log("Height:")
 // console.log(height  -4) //when it is - it always tries to convert string to a number 
 // console.log(parseInt(height)+2) //when it is plus it will just add number to an existing string , so you need to convert by parseInt. 
-console.log(height+2)
+console.log(height+2) //in this case you dont need parseInt because your var is a number not a string 
 
